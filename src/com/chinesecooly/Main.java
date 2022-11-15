018126167823ed9024e5a9793227a2787dbeec98
chinesecooly <chinesecooly@163.com>
2022-11-15 22:04:36 +0800

    init

--- src/com/chinesecooly/Main.java
+++ src/com/chinesecooly/Main.java
@@ -6,4 +6,39 @@ public class Main {
 	// write your code here
     }
 
+    private static String analyseRowType(){
+        return null;
+    }
+
+    private static String analyseAnnotation(){
+        return null;
+    }
+
+    private static String analyseModifier(){
+        return null;
+    }
+
+    private static String analyseSuperClass(){
+        return null;
+    }
+
+    private static String analyseSuperInterface(){
+        return null;
+    }
+
+    private static String analyseTypeParameter(){
+        return null;
+    }
+
+    private static String analyseField(){
+        return null;
+    }
+
+    private static String analyseConstructor(){
+        return null;
+    }
+
+    private static String analyseMethod(){
+        return null;
+    }
 }
