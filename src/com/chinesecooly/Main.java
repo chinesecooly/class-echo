d7f99121e7ee09d37eca4edf08129acfb9b07966
eric.ning <eric.ning@goertek.com>
2022-11-16 13:51:49 +0800

    大体框架

--- src/com/chinesecooly/Main.java
+++ src/com/chinesecooly/Main.java
@@ -1,44 +1,85 @@
 package com.chinesecooly;
 
+
+import java.util.Optional;
+import java.util.logging.Logger;
+
+/**
+ * @author eric.ning
+ */
 public class Main {
 
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+    private static final StringBuilder ECHO_CLASS = new StringBuilder();
+
     public static void main(String[] args) {
-	// write your code here
+        analyseRowType(String.class.getName())
+                .map(Main::analyseModifier);
+        LOGGER.info(ECHO_CLASS.toString());
     }
 
-    private static String analyseRowType(){
-        return null;
+    private static Optional<String> analyseRowType(String fullClassName) {
+        try {
+            Class<?> clazz = ClassLoader.getSystemClassLoader().loadClass(fullClassName);
+            if (clazz.isAnnotation()) {
+                append("@interface");
+                append(clazz.getSimpleName());
+            }else if (clazz.isEnum()){
+                append("enum");
+                append(clazz.getSimpleName());
+            }else if (clazz.isPrimitive()){
+                append(clazz.getSimpleName());
+                return Optional.empty();
+            }else if (clazz.isArray()){
+                append(clazz.getSimpleName());
+                return Optional.empty();
+            }else if (clazz.isInterface()){
+                append("interface");
+                append(clazz.getSimpleName());
+            }else {
+                append("class");
+                append(clazz.getSimpleName());
+            }
+        } catch (ClassNotFoundException e) {
+            LOGGER.info(e.getMessage());
+        }
+        return Optional.ofNullable(fullClassName);
     }
 
-    private static String analyseAnnotation(){
-        return null;
+    private static StringBuilder analyseAnnotation() {
+        return ECHO_CLASS;
     }
 
-    private static String analyseModifier(){
-        return null;
+    private static Optional<String> analyseModifier(String fullClassName) {
+        return Optional.of(fullClassName);
     }
 
-    private static String analyseSuperClass(){
-        return null;
+    private static StringBuilder analyseSuperClass() {
+        return ECHO_CLASS;
     }
 
-    private static String analyseSuperInterface(){
-        return null;
+    private static StringBuilder analyseSuperInterface() {
+        return ECHO_CLASS;
     }
 
-    private static String analyseTypeParameter(){
-        return null;
+    private static StringBuilder analyseTypeParameter() {
+        return ECHO_CLASS;
     }
 
-    private static String analyseField(){
-        return null;
+    private static StringBuilder analyseField() {
+        return ECHO_CLASS;
     }
 
-    private static String analyseConstructor(){
-        return null;
+    private static StringBuilder analyseConstructor() {
+        return ECHO_CLASS;
     }
 
-    private static String analyseMethod(){
-        return null;
+    private static StringBuilder analyseMethod() {
+        return ECHO_CLASS;
+    }
+
+    private static void append(String content){
+        ECHO_CLASS.append(content);
+        ECHO_CLASS.append(" ");
     }
 }
